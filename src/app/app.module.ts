9153a4752e078b70a89a420c2a19ba76e5c62849
= <pedropg.rojas@gmail.com>
2023-04-29 21:05:16 +0100

    progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ComponentsModule } from './components/components.module';
 import { PagesModule } from "./pages/pages.module";
 
 @NgModule({
@@ -17,7 +16,6 @@ import { PagesModule } from "./pages/pages.module";
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
-        ComponentsModule,
         PagesModule
     ]
 })
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
-
+import { ComponentsModule } from '../components/components.module';
+import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ComponentsModule,
+    MatIconModule,
+    MatButtonModule
   ],
   exports: [HomeComponent]
 })
