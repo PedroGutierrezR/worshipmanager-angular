c390724f1f312dd62c0aec17eb3b974a32bb73b4
= <pedropg.rojas@gmail.com>
2023-05-01 16:56:30 +0100

    details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PagesModule } from "./pages/pages.module";
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
     declarations: [
         AppComponent
     ],
-    providers: [],
+    providers: [{
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy
+    }],
     bootstrap: [AppComponent],
     imports: [
         BrowserModule,
