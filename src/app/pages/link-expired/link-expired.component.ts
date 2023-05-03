d381235304e5aa9ae044d6565ec09cfb000b94ba
= <pedropg.rojas@gmail.com>
2023-05-03 07:07:52 +0100

    lin-expired ready

--- src/app/pages/link-expired/link-expired.component.ts
+++ src/app/pages/link-expired/link-expired.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-link-expired',
   templateUrl: './link-expired.component.html',
   styleUrls: ['./link-expired.component.scss']
 })
-export class LinkExpiredComponent {
+export class LinkExpiredComponent  implements OnInit {
+
+  constructor(private router: Router){}
+
+  ngOnInit(): void {
+  }
+
+  goToHome() {
+    this.router.navigateByUrl('/home');
+  }
 
 }
